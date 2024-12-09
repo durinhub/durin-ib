969103b6639c008c00a5ef0114813e4bff6f7435
Durin <noreply@pudim.com>
2024-12-09 12:15:16 -0300

    add classe free text ao rodapé

--- resources/views/partials/_footer.blade.php
+++ resources/views/partials/_footer.blade.php
@@ -4,9 +4,9 @@
         <div class="col-sm-12 text-center footer-text">
             <img src="/storage/res/logo-ib.png">
             @if($configuracaos->url_repo)
-                <p>Este é um projeto de Código Aberto.</p>
-                <p>Confira <a href="{{ $configuracaos->url_repo }}" target="_blank">aqui</a> e contribua!</p>
-                <p>Tecnologias usadas: <a href="https://getbootstrap.com" target="_blank">Bootstrap</a> 
+                <p class="free-text">Este é um projeto de Código Aberto.</p>
+                <p class="free-text">Confira <a href="{{ $configuracaos->url_repo }}" target="_blank">aqui</a> e contribua!</p>
+                <p class="free-text">Tecnologias usadas: <a href="https://getbootstrap.com" target="_blank">Bootstrap</a> 
                 - <a href="https://jquery.com/" target="_blank">JQuery</a> 
                 - <a href="https://php.net/" target="_blank">PHP</a>
                 - <a href="https://laravel.com/" target="_blank">Laravel</a>
