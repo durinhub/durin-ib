ae585ff6c7fa8c984c711555231afa41a44d5084
Durin <noreply@pudim.com>
2024-12-30 19:07:37 -0300

    corrige redirecionamento inicio e fim página

--- resources/views/partials/_boardpostheader.blade.php
+++ resources/views/partials/_boardpostheader.blade.php
@@ -3,7 +3,7 @@
         <div class="col-sm-4"></div>
         <div class="col-sm-4 text-center">
         @include('partials._postform',  [ 'configuracaos' => $configuracaos ])
-        <a href="{{ Request::url() }}#div-footer">[Ir para o final da página]</a>
+        <a href="{{ Request::fullUrl() }}#div-footer">[Ir para o final da página]</a>
         </div>
         <div class="col-sm-4"></div>
     </div>
--- resources/views/partials/_divvoltatopo.blade.php
+++ resources/views/partials/_divvoltatopo.blade.php
@@ -1,6 +1,6 @@
 <div class="row">
     <div class="col-sm-3"></div>
-    <div class="col-sm-3"><a href="{{ Request::url() }}#div-form">[Voltar para o topo]</a></div>
+    <div class="col-sm-3"><a href="{{ Request::fullUrl() }}#div-form">[Voltar para o topo]</a></div>
     <div class="col-sm-3"></div>
     <div class="col-sm-3"></div>
 </div>
